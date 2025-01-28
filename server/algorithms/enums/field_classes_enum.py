eebefb77a9262da4f36f9ed30e78354cf6624176
Rud356 <r.moroshkin2015@gmail.com>
2025-01-28 21:19:14 +0300

    Add enums for predicted classes by detectron2

---
+++ server/algorithms/enums/field_classes_enum.py
@@ -0,0 +1,12 @@
+from enum import IntEnum
+
+
+class FieldClasses(IntEnum):
+    RedCenterLine = 0
+    BlueLine = 1
+    RedCircle = 2
+    GoalLine = 3
+    Field = 4
+    GoalZone = 5
+    Goal = 6
+    BlueCircle = 7
---
+++ server/algorithms/enums/player_classes_enum.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class PlayerClasses(IntEnum):
+    Player = 0
+    Referee = 1
+    Goalie = 2
