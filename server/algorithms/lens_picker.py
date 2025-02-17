5520309337be8fac1ec2f78015df2febe0c59a24
Rud356 <r.moroshkin2015@gmail.com>
2025-02-17 16:37:08 +0300

    Check correction example

--- server/algorithms/lens_picker.py
+++ server/algorithms/lens_picker.py
@@ -5,7 +5,10 @@ import ffmpeg
 # Initial values for k1 and k2
 k1 = -0.125
 k2 = 0.02
-hwaccel = "vulkan"
+
+k1=-0.225
+k2=0.025
+hwaccel = "auto"
 done = False
 
 # Skip frames to
@@ -13,16 +16,21 @@ done = False
 # start_frame_number = 50
 # For timestamps cv.CAP_PROP_POS_MSEC can be used
 # cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
-done = True
+done = False
+# ffmpeg.input("E:\Photo\Concert 26.09.2021\VID_20210926_214140.mp4", hwaccel=hwaccel).output("test_fetch_frame.mp4", frames="260", ss="15", preset="fast", crf="27", vf=f"scale=1280:720,lenscorrection=k1=-0.3223:k2=0.55").global_args("-y").run()
+
 
 while not done:
     (
-        ffmpeg.input("../../static/projects/441.jpeg", hwaccel=hwaccel)
-        .output("out.png", vf=f"scale=1280:720,lenscorrection=k1={k1}:k2={k2}")
+        ffmpeg.input(r"H:\ai\diploma_test\14-41-29 2023-01-15.mkv", hwaccel=hwaccel)
+        .output(
+            "converted_demo.mp4", vf=f"scale=1280:720,lenscorrection=k1={k1}:k2={k2}",
+            frames="1200", ss="15", preset="fast", crf="27",
+        )
         .global_args("-y")
         .run()
     )
-    image = cv2.imread("../../static/projects/out.png")
+    image = cv2.imread("out.png")
     cv2.imshow('Image', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -40,13 +48,14 @@ while not done:
     except ValueError:
         pass
 
-cap = cv2.VideoCapture("../../test_fetch_frame.mp4")
-while cap.isOpened():
-    ret, frame = cap.read()
+if not done:
+    cap = cv2.VideoCapture("../../test_fetch_frame.mp4")
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-    if not ret:
-        break
+        if not ret:
+            break
 
-    cv2.imshow("Frame", frame)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+        cv2.imshow("Frame", frame)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
