1ccaf1c580322889fcd3372fa93b505e8185fc69
Rud356 <r.moroshkin2015@gmail.com>
2025-04-03 03:04:42 +0300

    Replace with resnet18 based team detector that seem to work better

--- server/algorithms/nn/team_detector.py
+++ server/algorithms/nn/team_detector.py
@@ -3,8 +3,8 @@ from pathlib import Path
 
 import cv2
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.datasets as datasets
+from torchvision.models import resnet18
 
 from server.algorithms.enums.team import Team
 from server.algorithms.nn.team_detector_predictor import TeamDetectionPredictor
@@ -13,25 +13,15 @@ from server.algorithms.nn.team_detector_teacher import TeamDetectorTeacher, team
 
 class TeamDetectorModel(nn.Module):
     def __init__(self):
-        super(TeamDetectorModel, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(128 * 18 * 18, 512)
-        self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 2)  # Home team and away team
+        super().__init__()
+        self.resnet18 = resnet18(pretrained=True)
+        # Replace the final fully connected layer
+        num_ftrs = self.resnet18.fc.in_features
+        self.resnet18.fc = nn.Linear(num_ftrs, 2)  # Home team and away team
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = x.view(-1, 128 * 18 * 18)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = self.fc2(x)
+        x = self.resnet18(x)
         return x
-
 
 classes_names = list(Team.__members__)
 test_dir = '../datasets/custom_validation'
