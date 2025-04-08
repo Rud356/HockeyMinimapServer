ffab5c31a79d6a04c949357662635bbe2af412dd
Rud356 <r.moroshkin2015@gmail.com>
2025-04-08 17:32:59 +0300

    Add more type hints to fixtures in sqlalchemy tests

--- tests/test_sqlalchemy_impl/fixtures.py
+++ tests/test_sqlalchemy_impl/fixtures.py
@@ -1,18 +1,18 @@
 import pytest
 import pytest_asyncio  # noqa: used as plugin
 from dishka import Container, make_container
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
 from server.data_storage.sql_implementation.repository_sqla import RepositorySQLA, Repository
 from server.data_storage.sql_implementation.sqla_provider import SQLAlchemyProvider
 
 
 @pytest.fixture()
-def engine():
+def engine() -> AsyncEngine:
     return create_async_engine("sqlite+aiosqlite:///:memory:")
 
 @pytest.fixture()
-def provider(engine) -> SQLAlchemyProvider:
+def provider(engine: AsyncEngine) -> SQLAlchemyProvider:
     return SQLAlchemyProvider(engine)
 
 @pytest.fixture()
@@ -28,6 +28,5 @@ def repo(container: Container) -> RepositorySQLA:
     return repo
 
 @pytest.fixture(autouse=True)
-async def setup_db(engine, repo: RepositorySQLA):
-    print("DB Initialized")
+async def setup_db(engine: AsyncEngine, repo: RepositorySQLA):
     await repo.init_db(engine)
