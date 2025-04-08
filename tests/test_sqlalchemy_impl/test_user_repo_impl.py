24be95ed346f8a5aeeba0b60e2f31c7ce1899481
Rud356 <r.moroshkin2015@gmail.com>
2025-04-08 17:34:24 +0300

    Fix errors after testing about not cleaning up resources

--- tests/test_sqlalchemy_impl/test_user_repo_impl.py
+++ tests/test_sqlalchemy_impl/test_user_repo_impl.py
@@ -4,7 +4,8 @@ from server.data_storage.sql_implementation.tables import User
 from .fixtures import *
 
 async def test_db_init(repo):
-    await repo.transaction.session.execute(
-        Select(User)
-    )
+    async with repo.transaction as tr:
+        await tr.session.execute(
+            Select(User)
+        )
 
