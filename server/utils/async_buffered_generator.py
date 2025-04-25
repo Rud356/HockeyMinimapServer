aae1bf11335e6b52a9782edc9e10fa0a27700c86
Rud356 <r.moroshkin2015@gmail.com>
2025-04-25 17:36:37 +0300

    Add comments for async generators with buffering

--- server/utils/async_buffered_generator.py
+++ server/utils/async_buffered_generator.py
@@ -6,11 +6,25 @@ StreamedT = TypeVar("StreamedT")
 
 
 async def _fetch_data_into_buffer(queue: asyncio.Queue[StreamedT], stream: AsyncGenerator[StreamedT, None]) -> None:
+    """
+    Получает значения из генератора и помещает их в буфер.
+
+    :param queue: Очередь, реализующая буфер.
+    :param stream: Асинхронный генератор.
+    :return: Ничего.
+    """
     async for data in stream:
         await queue.put(data)
 
 
 async def buffered_generator(stream: AsyncGenerator[StreamedT, None], size: int) -> AsyncGenerator[StreamedT, None]:
+    """
+    Создает буферизованный итератор из асинхронного генератора.
+
+    :param stream: Исходный генератор.
+    :param size: Размер буфера.
+    :return: Буферизованный генератор.
+    """
     buffer: asyncio.Queue[StreamedT] = asyncio.Queue(size)
     buffer_filling_task: asyncio.Task[None] = asyncio.create_task(
         _fetch_data_into_buffer(buffer, stream)
